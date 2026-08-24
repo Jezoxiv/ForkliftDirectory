a39e243bf955e46fb7d1c695a5fb772289aa3b5b
Jezoxiv <75989875+Jezoxiv@users.noreply.github.com>
2026-08-24 18:31:12 +0300

    Update Forklift.cs

--- ForkliftDirectory/Models/Forklift.cs
+++ ForkliftDirectory/Models/Forklift.cs
@@ -7,7 +7,8 @@ public class Forklift
     public string Brand { get; set; } = string.Empty;
 
     public string Number { get; set; } = string.Empty;
-
+    
+    [Column(TypeName = "numeric(10,3)")]
     public decimal LoadCapacity { get; set; }
 
     public bool Active { get; set; } = true;
